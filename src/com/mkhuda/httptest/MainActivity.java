e8daac5039387ea064af42867ef0661c78fc5f8c
mkhuda <mhmmd.huda@gmail.com>
2015-09-22 22:19:21 +0700

    Formatting

--- src/com/mkhuda/httptest/MainActivity.java
+++ src/com/mkhuda/httptest/MainActivity.java
@@ -28,92 +28,95 @@ import org.apache.http.impl.client.DefaultHttpClient;
 
 @SuppressLint("NewApi")
 public class MainActivity extends ActionBarActivity {
-	
-    TextView textJohn;
-    TextView textKoneksi;
-	
+
+	TextView textJohn;
+	TextView textKoneksi;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		textJohn = (TextView) findViewById(R.id.john);
 		textKoneksi = (TextView) findViewById(R.id.connect);
-		
-		if(isConnected()){
+
+		if (isConnected()) {
 			textKoneksi.setBackgroundColor(0xFF00CC00);
 			textKoneksi.setText("You are connected");
-        }
-        else{
-        	textKoneksi.setBackgroundColor(0x5b7ca500);
-        	textKoneksi.setText("You are NOT connected");
-        }
-		
+		} else {
+			textKoneksi.setBackgroundColor(0x5b7ca500);
+			textKoneksi.setText("You are NOT connected");
+		}
+
 		new HttpAsyncTask().execute("http://androgan.com/test.php");
-		
+
 	}
-	
-	public static String GET(String url){
-        InputStream inputStream = null;
-        String result = "";
-        try {
- 
-            // create HttpClient
-            HttpClient httpclient = new DefaultHttpClient();
- 
-            // make GET request to the given URL
-            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
- 
-            // receive response as inputStream
-            inputStream = httpResponse.getEntity().getContent();
- 
-            // convert inputstream to string
-            if(inputStream != null)
-                result = convertInputStreamToString(inputStream);
-            else
-                result = "Did not work!";
- 
-        } catch (Exception e) {
-            Log.d("InputStream", e.getLocalizedMessage());
-        }
- 
-        return result;
-    }
-	
-	private static String convertInputStreamToString(InputStream inputStream) throws IOException{
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while((line = bufferedReader.readLine()) != null)
-            result += line;
- 
-        inputStream.close();
-        return result;
- 
-    }
-	
-	public boolean isConnected(){
-        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-            if (networkInfo != null && networkInfo.isConnected()) 
-                return true;
-            else
-                return false;   
-    }
-	
+
+	public static String GET(String url) {
+		InputStream inputStream = null;
+		String result = "";
+		try {
+
+			// create HttpClient
+			HttpClient httpclient = new DefaultHttpClient();
+
+			// make GET request to the given URL
+			HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+			// receive response as inputStream
+			inputStream = httpResponse.getEntity().getContent();
+
+			// convert inputstream to string
+			if (inputStream != null)
+				result = convertInputStreamToString(inputStream);
+			else
+				result = "Did not work!";
+
+		} catch (Exception e) {
+			Log.d("InputStream", e.getLocalizedMessage());
+		}
+
+		return result;
+	}
+
+	private static String convertInputStreamToString(InputStream inputStream)
+			throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(
+				new InputStreamReader(inputStream));
+		String line = "";
+		String result = "";
+		while ((line = bufferedReader.readLine()) != null)
+			result += line;
+
+		inputStream.close();
+		return result;
+
+	}
+
+	public boolean isConnected() {
+		ConnectivityManager connMgr = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+		if (networkInfo != null && networkInfo.isConnected())
+			return true;
+		else
+			return false;
+	}
+
 	private class HttpAsyncTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... urls) {
- 
-            return GET(urls[0]);
-        }
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(String result) {
-            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
-            textJohn.setText(result);
-       }
-    }
+		@Override
+		protected String doInBackground(String... urls) {
+
+			return GET(urls[0]);
+		}
+
+		// onPostExecute displays the results of the AsyncTask.
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG)
+					.show();
+			textJohn.setText(result);
+		}
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
